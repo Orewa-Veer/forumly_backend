458d02541237cdc3b8fd18d419be5b08ab65c567
Orewa-veer <pratiharvirendra@gmail.com>
2025-07-27 17:33:43 +0530

    fix: run a script to update questonCounter on tags

---
+++ updateTags.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { Tag } from "./models/tags.js";
+import { Discussion } from "./models/Discussion.js";
+import logger from "./middleware/logger.js";
+await mongoose.connect("mongodb://localhost/formuly").catch((ex) => {
+  logger.error("Could not connect to MongoDB: ", ex);
+  process.exit(1);
+});
+
+const tags = await Tag.find();
+for (const tag of tags) {
+  const count = await Discussion.countDocuments({ "tags._id": tag._id });
+  await Tag.updateOne({ _id: tag._id }, { $set: { questionCounter: count } });
+}
+console.log("Tags questionCounter Updated");
+
+await mongoose.disconnect();
